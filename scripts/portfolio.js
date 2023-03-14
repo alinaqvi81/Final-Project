4e87aa5d2b298fa67c118f3c78decbd743d5e078
alinaqvi81 <127545216+alinaqvi81@users.noreply.github.com>
2023-03-14 16:47:48 -0600

    Add files via upload

---
+++ scripts/portfolio.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Full Stack Developer", "Petroleum Engineer", "Drilling Eng Specialist","Project Manager","Data Analyt"," Business Manager", ""],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
